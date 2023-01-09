ce643c62911c70ba670c3ba685560c3d740c23e4
OARosique <oarosique@interceramic.com>
2023-01-08 18:12:40 -0600

    reestructuración de código

--- notificaciones_push_ejemplo/lib/main.dart
+++ notificaciones_push_ejemplo/lib/main.dart
@@ -1,31 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:notificaciones_push_ejemplo/screens/main_screen.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-      
-        primarySwatch: Colors.blue,
-      ),
-      home: const MainScreen()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MainScreen());
   }
 }
-
- 
